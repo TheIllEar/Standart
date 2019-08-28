92095a7012ba43eb7b579a179df52e768cabf0a7
Glushak Igor <theillear@gmail.com>
2019-08-28 15:42:03 +0300

    Standart

---
+++ script/script.js
@@ -0,0 +1,5 @@
+//Допустим у нас 7 страниц и на каждую есть изображение
+
+// let value = document.querySelector("img");
+// let valueNum = value.getAttribute("class")[8];// 8 символов в классе
+// value.style.backgroundImage="url(img/"+ valueNum +".jpg)"
